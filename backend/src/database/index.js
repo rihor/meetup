0602706c0dc3527f17bb4091a78e3f731c73a599
Pedro Pinho <rihor1000@gmail.com>
2019-08-16 12:14:28 -0300

    Removendo conexão não utilizada

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,5 +1,4 @@
 import Sequelize from 'sequelize';
-import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -14,7 +13,6 @@ class Database {
     this.connection = new Sequelize(databaseConfig);
     this.init();
     this.associate();
-    this.mongo();
   }
 
   init() {
@@ -26,13 +24,6 @@ class Database {
       if (model.associate) {
         model.associate(this.connection.models);
       }
-    });
-  }
-
-  mongo() {
-    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useFindAndModify: true,
     });
   }
 }
